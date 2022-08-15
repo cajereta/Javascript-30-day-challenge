4817680d1954cb3feb2651d4eac2102e5fda466f
Cajereta <msijuicebox@gmail.com>
2022-08-15 04:30:36 -0300

    Added day 2.

---
+++ Day2/app.js
@@ -0,0 +1,17 @@
+const hourHand = document.querySelector(".hour-hand");
+const minHand = document.querySelector(".min-hand");
+const secondHand = document.querySelector(".second-hand");
+function setDate(){
+   const now = new Date();
+   const seconds = now.getSeconds();
+   const secondsDegrees = ((seconds/60)*360)+90;
+   const minutes = now.getMinutes();
+   const minutesDegrees = ((minutes/60)*360)+90;
+   const hours = now.getHours();
+   const hoursDegrees = ((hours/12)*360)+90;
+   
+   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
+setInterval(setDate,1000);
